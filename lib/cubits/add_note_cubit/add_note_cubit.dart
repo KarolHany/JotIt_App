4ea925ef705938c342b1cfa4bc461a92b877d4f4
karol <karolhany09@gmail.com>
2024-09-24 22:01:48 +0300

    add addNote method to addNote cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:jotit_app/constants.dart';
 import 'package:jotit_app/models/note_model.dart';
 import 'package:meta/meta.dart';
 
@@ -7,9 +9,16 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
-  addNote(NoteModel note){
-    
+ // Add Method
+  addNote(NoteModel note) async {
+    try {
+      emit(AddNoteLoading());
+      var noteBox = Hive.box(kNotesBox);
+      await noteBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(errorMessage: e.toString()));
+    }
   }
+  
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:jotit_app/views/note_view.dart';
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
-  Hive.registerAdapter(NoteModelAdapter());
+  Hive.registerAdapter(NoteModelAdapter()); // guide app to use hive with NoteModel (i choose the udapter that i want to hive use it in my app)
   runApp(const NoteApp());
 }
 
