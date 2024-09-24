be87c9662f21e7c9aeffaacabf8b681256aca378
karol <karolhany09@gmail.com>
2024-09-25 01:17:40 +0300

    provide addNoteCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:jotit_app/constants.dart';
+import 'package:jotit_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:jotit_app/models/note_model.dart';
 import 'package:jotit_app/views/note_view.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
-  Hive.registerAdapter(NoteModelAdapter()); // guide app to use hive with NoteModel (i choose the udapter that i want to hive use it in my app)
+  Hive.registerAdapter(
+      NoteModelAdapter()); // guide app to use hive with NoteModel (i choose the udapter that i want to hive use it in my app)
   runApp(const NoteApp());
 }
 
@@ -16,14 +19,19 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        title: 'JOTIT',
+        home: const NoteView(),
       ),
-      title: 'JOTIT',
-      home: const NoteView(),
     );
   }
 }
